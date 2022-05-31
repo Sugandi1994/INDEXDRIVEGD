c86004d01e533faa3b6b6903716352b112e8ad0d
SugandiAnarki <64920399+SugandiAnarki@users.noreply.github.com>
2022-05-31 13:35:21 +0700

    Update app2.js

--- themes/material/app2.js
+++ themes/material/app2.js
@@ -778,7 +778,7 @@ function file_video(path) {
     .map(it => `<li class="mdui-menu-item"><a href="${it.href}" class="mdui-ripple">${it.text}</a></li>`)
     .join('');
   player_items += `<li class="mdui-divider"></li>
-                   <li class="mdui-menu-item"><a id="copy-link" class="mdui-ripple">Copy Link</a></li>`;
+                   <!--<li class="mdui-menu-item"><a id="copy-link" class="mdui-ripple">Copy Link</a></li>-->`;
   const playBtn = `
       <button class="mdui-btn mdui-ripple mdui-color-theme-accent" mdui-menu="{target:'#player-items'}">
         <i class="mdui-icon material-icons">&#xe039;</i>Play from external player<i class="mdui-icon material-icons">&#xe5cf;</i>
